ec7bfe5bed9f4510ac493a818feba94753c1474e
Mario Lizier <lizier@gmail.com>
2023-06-07 16:25:49 -0300

    Update install.sh

--- tx9-hotspot/install.sh
+++ tx9-hotspot/install.sh
@@ -15,7 +15,20 @@ echo "#!/bin/bash" > /root/wifi.sh
 echo "[[ -n \$(grep led-conf5 /boot/armbianEnv.txt) ]] && { sed -i 's/led-conf5/led-conf4/' /boot/armbianEnv.txt; sleep 5s; reboot; } || { sed -i 's/led-conf4/led-conf5/' /boot/armbianEnv.txt; }" >> /root/wifi.sh
 chmod +x /root/wifi.sh
 
+apt update
+apt install dnsmasq -y
 
+systemctl stop dnsmasq
+systemctl disable dnsmasq
+
+SSID="hotspot"
+PWD="hotspot1234"
+
+nmcli con add type wifi ifname wlan0 con-name $SSID autoconnect yes ssid $SSID
+nmcli con modify $SSID 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+nmcli con modify $SSID wifi-sec.key-mgmt wpa-psk
+nmcli con modify $SSID wifi-sec.psk $PWD
+nmcli con up $SSID
 
 
 
